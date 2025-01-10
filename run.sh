28664270505a688e0c82bcde343e4b3a173108e5
mwlasiuk <testmailsmtp12345@gmail.com>
2025-01-10 19:24:40 +0100

    Run via run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+./build/pbrt /home/michal/code/pbrt-v4-scenes/calibration/geometry_calibration/0.pbrt
+./build/pbrt /home/michal/code/pbrt-v4-scenes/calibration/geometry_calibration/1.pbrt
+./build/pbrt /home/michal/code/pbrt-v4-scenes/calibration/geometry_calibration/2.pbrt
+./build/pbrt /home/michal/code/pbrt-v4-scenes/calibration/geometry_calibration/3.pbrt
+./build/pbrt /home/michal/code/pbrt-v4-scenes/calibration/geometry_calibration/4.pbrt
+./build/pbrt /home/michal/code/pbrt-v4-scenes/calibration/geometry_calibration/5.pbrt
